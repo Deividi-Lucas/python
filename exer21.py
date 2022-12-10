0dad7e235a6ac2cbfe78aab68841e66ab802b27d
Deividi_Lucas <113848434+Deividi-Lucas@users.noreply.github.com>
2022-12-10 17:54:12 -0400

    Play music

---
+++ exer21.py
@@ -0,0 +1,12 @@
+import playsound
+
+playsound.playsound('burn.mp3')
+
+# Para eventual mudança usar esse de baixo. 
+
+#import pygame
+#pygame.init()
+#pygame.mixer.init()
+#pygame.mixer.music.load('burn.mp3')
+#pygame.mixer.music.play(loops=0)
+#pygame.event.wait()
